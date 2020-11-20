09239a842d7e6d70c731b60fd4ffb3b1a55b8954
Loic-MEHAL <mehal.loic@gmail.com>
2020-11-20 13:35:03 +0100

    vérificatuin email en bdd

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -20,8 +20,14 @@ class UserController extends AbstractController
                     'id_verif' => 1,
                     'id_cripte' => $_POST['email']
                 ];
+            $verify = $userManager->verify();
+            $error = 'Mail exist';
+            if ($_POST['email']!= $verify['email']) {
             $id = $userManager->addUser($user);
             header('Location:/User/showUser/' . $id);
+            }else{
+                return $this->twig->render('User/addUser.html.twig', ['error' => $error]);
+            }
         } else {
             return $this->twig->render('User/addUser.html.twig');
         }
